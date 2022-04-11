a4563c21580afd1ec9448ac7b18e056181057152
Topher <TopherBirth@Gmail.com>
2022-04-11 15:58:46 -0500

    testing something

--- ConsoleChallenges/Program.cs
+++ ConsoleChallenges/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleChallenges
 {
@@ -12,7 +13,43 @@ namespace ConsoleChallenges
             //fb.ThirdTry(25);
             //fb.FourthTry(25);
             //WordSearch.Go();
-            Qwerty.Go("woordy");
+            //Qwerty.Go("woordy");
+            var result = getHeaviestPackage(new List<int> { 4, 30, 15, 5, 9 });
+        }
+
+        /// <summary>
+        /// graph TD
+        ///     A[Christmas] -->|Get money| B(Go shopping)
+        ///     B --> C{Let me think}
+        ///     C -->|One| D[Laptop]
+        ///     C -->|Two| E[iPhone]
+        ///     C -->|Three| F[fa:fa - car Car]
+        /// </summary>
+        /// <param name="packageWeights"></param>
+        /// <returns></returns>
+        static long getHeaviestPackage(List<int> packageWeights)
+        {
+            var compare = 0;
+            while (packageWeights.Count != compare)
+            {
+                compare = packageWeights.Count;
+                for (int instance = 0; instance < packageWeights.Count; instance++)
+                {
+                    if (instance + 1 > packageWeights.Count - 1) break;
+                    if (packageWeights[instance] < packageWeights[instance + 1])
+                    {
+                        packageWeights[instance] = packageWeights[instance] + packageWeights[instance + 1];
+                        packageWeights.RemoveAt(instance + 1);
+                    }
+                }
+            }
+
+            var largest = 0;
+            foreach (var package in packageWeights)
+            {
+                if (package >= largest) largest = package;
+            }
+            return largest;
         }
     }
 }
---
+++ ConsoleChallenges/readme.md
@@ -0,0 +1,6 @@
+﻿graph TD
+    A[Christmas] -->|Get money| B(Go shopping)
+    B --> C{Let me think}
+    C -->|One| D[Laptop]
+    C -->|Two| E[iPhone]
+    C -->|Three| F[fa:fa-car Car]
